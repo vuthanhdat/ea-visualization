ad690210f01cbb9290694ff99919ef40f54541f7
vuthanhdat <vudat.fami@gmail.com>
2026-09-15 19:28:00 +0700

    feat: enable semantic drill-down demo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import AppV2 from './AppV2';
 import './styles.css';
+import './drilldown.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppV2 />
   </StrictMode>,
 );
